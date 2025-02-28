da9b9dd4bff30aa6827397dcf0f38e478a774c39
adrian <w0513060@campus.nscc.ca>
2025-02-28 10:22:21 -0400

    added functionality to support all non-express routes in the hrm

--- main.js
+++ main.js
@@ -21,11 +21,46 @@
         <label><input type="checkbox" name="route" value="3" checked> Route 3</label><br>
         <label><input type="checkbox" name="route" value="4" checked> Route 4</label><br>
         <label><input type="checkbox" name="route" value="5" checked> Route 5</label><br>
-        <label><input type="checkbox" name="route" value="6" checked> Route 6</label><br>
-        <label><input type="checkbox" name="route" value="7" checked> Route 7</label><br>
+        <label><input type="checkbox" name="route" value="6" checked> Route 6 (A/B/C)</label><br>
+        <label><input type="checkbox" name="route" value="7" checked> Route 7 (A/B)</label><br>
         <label><input type="checkbox" name="route" value="8" checked> Route 8</label><br>
-        <label><input type="checkbox" name="route" value="9" checked> Route 9</label><br>
-        <label><input type="checkbox" name="route" value="10" checked> Route 10</label>
+        <label><input type="checkbox" name="route" value="9" checked> Route 9 (A/B)</label><br>
+        <label><input type="checkbox" name="route" value="10" checked> Route 10 (A/B/C)</label><br>
+        <label><input type="checkbox" name="route" value="21" checked> Route 21</label><br>
+        <label><input type="checkbox" name="route" value="22" checked> Route 22</label><br>
+        <label><input type="checkbox" name="route" value="24" checked> Route 24</label><br>
+        <label><input type="checkbox" name="route" value="25" checked> Route 25</label><br>
+        <label><input type="checkbox" name="route" value="26" checked> Route 26</label><br>
+        <label><input type="checkbox" name="route" value="28" checked> Route 28</label><br>
+        <label><input type="checkbox" name="route" value="29" checked> Route 29</label><br>
+        <label><input type="checkbox" name="route" value="30" checked> Route 30 (A/B)</label><br>
+        <label><input type="checkbox" name="route" value="39" checked> Route 39</label><br>
+        <label><input type="checkbox" name="route" value="50" checked> Route 50</label><br>
+        <label><input type="checkbox" name="route" value="51" checked> Route 51 (A/B)</label><br>
+        <label><input type="checkbox" name="route" value="53" checked> Route 53</label><br>
+        <label><input type="checkbox" name="route" value="54" checked> Route 54</label><br>
+        <label><input type="checkbox" name="route" value="55" checked> Route 55</label><br>
+        <label><input type="checkbox" name="route" value="56" checked> Route 56</label><br>
+        <label><input type="checkbox" name="route" value="58" checked> Route 58</label><br>
+        <label><input type="checkbox" name="route" value="59" checked> Route 59</label><br>
+        <label><input type="checkbox" name="route" value="61" checked> Route 61</label><br>
+        <label><input type="checkbox" name="route" value="62" checked> Route 62</label><br>
+        <label><input type="checkbox" name="route" value="63" checked> Route 63</label><br>
+        <label><input type="checkbox" name="route" value="64" checked> Route 64</label><br>
+        <label><input type="checkbox" name="route" value="65" checked> Route 65</label><br>
+        <label><input type="checkbox" name="route" value="67" checked> Route 67</label><br>
+        <label><input type="checkbox" name="route" value="68" checked> Route 68 (A/B)</label><br>
+        <label><input type="checkbox" name="route" value="72" checked> Route 72</label><br>
+        <label><input type="checkbox" name="route" value="82" checked> Route 82</label><br>
+        <label><input type="checkbox" name="route" value="83" checked> Route 83</label><br>
+        <label><input type="checkbox" name="route" value="84" checked> Route 84</label><br>
+        <label><input type="checkbox" name="route" value="85" checked> Route 85</label><br>
+        <label><input type="checkbox" name="route" value="86" checked> Route 86</label><br>
+        <label><input type="checkbox" name="route" value="87" checked> Route 87</label><br>
+        <label><input type="checkbox" name="route" value="88" checked> Route 88</label><br>
+        <label><input type="checkbox" name="route" value="90" checked> Route 90</label><br>
+        <label><input type="checkbox" name="route" value="91" checked> Route 91</label><br>
+        <label><input type="checkbox" name="route" value="93" checked> Route 93</label>
       </form>`;
     
     return div;
@@ -45,9 +80,9 @@
   // initialize icon images for all the busses, add them into an array
   const icons = {};
 
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 93; i++) {
     icons[i] = L.icon({
-      iconUrl: `bus${i}.png`,
+      iconUrl: `bus icons/bus${i}.png`,
       iconSize: [16, 40],
       iconAnchor: [8, 20]
     });
@@ -96,7 +131,7 @@
           // filtering all the bus routes out so we only keep 1 - 10
           const filteredFeatures = geojson.features.filter(feature => {
               const routeNumber = parseInt(feature.properties.routeNumber);
-              return (routeNumber >= 1 && routeNumber <= 10);
+              return (routeNumber >= 1 && routeNumber <= 93);
           });
   
 
@@ -108,7 +143,11 @@
             const tripID = feature.properties.tripID;
 
             const routeNumber = parseInt(feature.properties.routeNumber);
-            const chosenIcon = icons[routeNumber];
+
+            for (let i = 1; i <= 93; i++) {
+              chosenIcon = icons[routeNumber]
+            }
+
             const parsedRoutes = selectedRoutes.map(item => parseInt(item));
 
             // if the busses route is one of the ones the user has selected: 
