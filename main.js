59514a32d334339eb2f4b43506b7b67467eb3235
adrian <w0513060@campus.nscc.ca>
2025-03-02 19:13:45 -0400

    made the side menu look slightly nicer

--- main.js
+++ main.js
@@ -10,55 +10,57 @@
   // adding the checkboxes to the top right
   const routeControl = L.control({ position: 'topright' });
 
-  routeControl.onAdd = function(map) {
+  routeControl.onAdd = function() {
     const div = L.DomUtil.create('div', 'route-control leaflet-bar');
     
     // creates a div containing the checkboxes
     div.innerHTML =
      `<form id="routeFilterForm" style="padding: 10px; background-color:white">
-        <label><input type="checkbox" name="route" value="1" checked> Route 1</label><br>
+        <input type="button" id="button" onClick="checkAll(true);" value="Check All">
+        <input type="button" id="button" onClick="checkAll(false);" value="Clear All"><br>
+        <label><input type="checkbox" name="route" value="1" checked> Route 1</label>
         <label><input type="checkbox" name="route" value="2" checked> Route 2</label><br>
-        <label><input type="checkbox" name="route" value="3" checked> Route 3</label><br>
+        <label><input type="checkbox" name="route" value="3" checked> Route 3</label>
         <label><input type="checkbox" name="route" value="4" checked> Route 4</label><br>
-        <label><input type="checkbox" name="route" value="5" checked> Route 5</label><br>
-        <label><input type="checkbox" name="route" value="6" checked> Route 6 (A/B/C)</label><br>
-        <label><input type="checkbox" name="route" value="7" checked> Route 7 (A/B)</label><br>
+        <label><input type="checkbox" name="route" value="5" checked> Route 5</label>
+        <label><input type="checkbox" name="route" value="6" checked> Route 6</label><br>
+        <label><input type="checkbox" name="route" value="7" checked> Route 7</lable>
         <label><input type="checkbox" name="route" value="8" checked> Route 8</label><br>
-        <label><input type="checkbox" name="route" value="9" checked> Route 9 (A/B)</label><br>
-        <label><input type="checkbox" name="route" value="10" checked> Route 10 (A/B/C)</label><br>
-        <label><input type="checkbox" name="route" value="21" checked> Route 21</label><br>
+        <label><input type="checkbox" name="route" value="9" checked> Route 9</label>
+        <label><input type="checkbox" name="route" value="10" checked> Route 10</label><br>
+        <label><input type="checkbox" name="route" value="21" checked> Route 21</label>
         <label><input type="checkbox" name="route" value="22" checked> Route 22</label><br>
-        <label><input type="checkbox" name="route" value="24" checked> Route 24</label><br>
+        <label><input type="checkbox" name="route" value="24" checked> Route 24</label>
         <label><input type="checkbox" name="route" value="25" checked> Route 25</label><br>
-        <label><input type="checkbox" name="route" value="26" checked> Route 26</label><br>
+        <label><input type="checkbox" name="route" value="26" checked> Route 26</label>
         <label><input type="checkbox" name="route" value="28" checked> Route 28</label><br>
-        <label><input type="checkbox" name="route" value="29" checked> Route 29</label><br>
-        <label><input type="checkbox" name="route" value="30" checked> Route 30 (A/B)</label><br>
-        <label><input type="checkbox" name="route" value="39" checked> Route 39</label><br>
+        <label><input type="checkbox" name="route" value="29" checked> Route 29</label>
+        <label><input type="checkbox" name="route" value="30" checked> Route 30</label><br>
+        <label><input type="checkbox" name="route" value="39" checked> Route 39</label>
         <label><input type="checkbox" name="route" value="50" checked> Route 50</label><br>
-        <label><input type="checkbox" name="route" value="51" checked> Route 51 (A/B)</label><br>
+        <label><input type="checkbox" name="route" value="51" checked> Route 51</label>
         <label><input type="checkbox" name="route" value="53" checked> Route 53</label><br>
-        <label><input type="checkbox" name="route" value="54" checked> Route 54</label><br>
+        <label><input type="checkbox" name="route" value="54" checked> Route 54</label>
         <label><input type="checkbox" name="route" value="55" checked> Route 55</label><br>
-        <label><input type="checkbox" name="route" value="56" checked> Route 56</label><br>
+        <label><input type="checkbox" name="route" value="56" checked> Route 56</label>
         <label><input type="checkbox" name="route" value="58" checked> Route 58</label><br>
-        <label><input type="checkbox" name="route" value="59" checked> Route 59</label><br>
+        <label><input type="checkbox" name="route" value="59" checked> Route 59</label>
         <label><input type="checkbox" name="route" value="61" checked> Route 61</label><br>
-        <label><input type="checkbox" name="route" value="62" checked> Route 62</label><br>
+        <label><input type="checkbox" name="route" value="62" checked> Route 62</label>
         <label><input type="checkbox" name="route" value="63" checked> Route 63</label><br>
-        <label><input type="checkbox" name="route" value="64" checked> Route 64</label><br>
+        <label><input type="checkbox" name="route" value="64" checked> Route 64</label>
         <label><input type="checkbox" name="route" value="65" checked> Route 65</label><br>
-        <label><input type="checkbox" name="route" value="67" checked> Route 67</label><br>
-        <label><input type="checkbox" name="route" value="68" checked> Route 68 (A/B)</label><br>
-        <label><input type="checkbox" name="route" value="72" checked> Route 72</label><br>
+        <label><input type="checkbox" name="route" value="67" checked> Route 67</label>
+        <label><input type="checkbox" name="route" value="68" checked> Route 68</label><br>
+        <label><input type="checkbox" name="route" value="72" checked> Route 72</label>
         <label><input type="checkbox" name="route" value="82" checked> Route 82</label><br>
-        <label><input type="checkbox" name="route" value="83" checked> Route 83</label><br>
+        <label><input type="checkbox" name="route" value="83" checked> Route 83</label>
         <label><input type="checkbox" name="route" value="84" checked> Route 84</label><br>
-        <label><input type="checkbox" name="route" value="85" checked> Route 85</label><br>
+        <label><input type="checkbox" name="route" value="85" checked> Route 85</label>
         <label><input type="checkbox" name="route" value="86" checked> Route 86</label><br>
-        <label><input type="checkbox" name="route" value="87" checked> Route 87</label><br>
+        <label><input type="checkbox" name="route" value="87" checked> Route 87</label>
         <label><input type="checkbox" name="route" value="88" checked> Route 88</label><br>
-        <label><input type="checkbox" name="route" value="90" checked> Route 90</label><br>
+        <label><input type="checkbox" name="route" value="90" checked> Route 90</label>
         <label><input type="checkbox" name="route" value="91" checked> Route 91</label><br>
         <label><input type="checkbox" name="route" value="93" checked> Route 93</label>
       </form>`;
@@ -76,6 +78,26 @@
       selectedRoutes = [...document.querySelectorAll("#routeFilterForm input[name='route']:checked")]
           .map(checkbox => checkbox.value);
   });
+
+  // Function to update the selectedRoutes variable based on currently checked boxes.
+  function updateSelectedRoutes() {
+    selectedRoutes = [...document.querySelectorAll("#routeFilterForm input[name='route']:checked")]
+        .map(checkbox => checkbox.value);
+    // Optionally, do something with selectedRoutes here (like re-filtering your data).
+  }
+
+  // Modify checkAll to call updateSelectedRoutes once it has finished toggling checkboxes.
+  window.checkAll = function(checkEm) {
+    const checkboxes = document.getElementsByTagName('input');
+    for (let i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].type === 'checkbox' && checkboxes[i].name === 'route') {
+        checkboxes[i].checked = checkEm;
+      }
+    }
+    // Update the selectedRoutes variable after changing the checkboxes.
+    updateSelectedRoutes();
+  };
+
 
   // initialize icon images for all the busses, add them into an array
   const icons = {};
